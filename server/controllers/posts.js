40c8c0b575491e3d8fe810e2abc8d1312aa4e6ef
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2023-01-17 02:15:59 -0500

    completed likePost controller and route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -24,6 +24,54 @@ export const createPost = async (req, res) => {
     const posts = await Post.find();
     res.status(200).json(posts);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    res
+      .status(409)
+      .json({ message: "Post was not created ❌", error: error.message });
+  }
+};
+
+// Read
+const getFeedPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+};
+const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const posts = await Post.find({ userId });
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+};
+
+// Update
+
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(404).json(error.message);
   }
 };
