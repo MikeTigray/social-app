1695bbf75e8fb989ee424590820fae9d303d5866
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2023-01-16 19:18:54 -0500

    completed getUser and getUserFriends controllers

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 // Configurations for using type modules
@@ -43,6 +44,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 // Routes
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // Mongoose Setup
 const PORT = process.env.PORT || 6001;
