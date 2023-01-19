6a64975aef97129f999b06afd265c12bda1ff8c2
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2023-01-18 23:36:05 -0500

    imported icons and styled components from mui for navbar

---
+++ client/src/pages/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import flexBetween from "components/flexBetween";
+
+function Navbar() {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  //   Themes
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  //   Convenience variable
+  const fullName = `${user.firstName} ${user.lastName}`;
+  return <div></div>;
+}
+
+export default Navbar;
